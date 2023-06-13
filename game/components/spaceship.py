a4ef6267d362d78ae97713773a06df23a03cd6b4
Sebastian Perez Orozco <136299407+SebastianPerezOrozco@users.noreply.github.com>
2023-06-13 00:59:14 -0500

    HOMEWORK-1
    He desarrollado la transición de la nave en el eje-x.

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -11,41 +11,41 @@ class Spaceship(Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 520
         self.rect.y = 500
-    
+
     def move_left(self):
-        if self.rect.left > 0:
-            self.rect.x -= 10
-        
-    
+        self.rect.x -= 10
+        # if self.rect.right < 0:
+        #     self.rect.x = SCREEN_WIDTH
+
     def move_right(self):
-        if self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10
+        self.rect.x += 10
+        # if self.rect.left > SCREEN_WIDTH:
+        #     self.rect.x = 0
 
     def move_up(self):
-         if self.rect.y > SCREEN_HEIGHT // 2:
+        if self.rect.y > SCREEN_HEIGHT // 2:
             self.rect.y -= 10
 
     def move_down(self):
-         if self.rect.y < SCREEN_HEIGHT - 10:
+        if self.rect.y < SCREEN_HEIGHT - 50:
             self.rect.y += 10
 
-
     def update(self, user_input):
+
         if user_input[pygame.K_LEFT]:
             self.move_left()
-
         elif user_input[pygame.K_RIGHT]:
             self.move_right()
-
         elif user_input[pygame.K_UP]:
             self.move_up()
-
         elif user_input[pygame.K_DOWN]:
-            self.move_down() 
+            self.move_down()
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-        
 
+        if self.rect.left < 0:
+            screen.blit(self.image, (self.rect.left + SCREEN_WIDTH, self.rect.y))
 
-
+        elif self.rect.right > SCREEN_WIDTH:
+            screen.blit(self.image, (self.rect.left - SCREEN_WIDTH, self.rect.y))
